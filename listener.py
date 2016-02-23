aeccd36b7ff155f6ede6500967f827871983a8e6
Ben Kamens <ben@khanacademy.org>
2016-02-22 22:42:02 -0800

    Start to add ability to listen to slack rooms via slack bot. Test plan: none

---
+++ listener.py
@@ -0,0 +1,23 @@
+"""STOPSHIP"""
+import logging
+import time
+
+import slackclient
+
+import secrets
+
+
+def listen():
+    """STOPSHIP"""
+    client = slackclient.SlackClient(
+            secrets.slack_testimonials_turtle_api_token)
+    if not client.rtm_connect():
+        logging.critical("Failed to connect to Slack RTM API, bailing.")
+        return
+
+    # STOPSHIP: explanation
+    while True:
+        messages = client.rtm_read()
+        logging.critical(messages)
+        time.sleep(1)
+
