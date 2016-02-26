da49502619a03d933876ca5da64e1c13b0ff8285
Ben Kamens <ben@khanacademy.org>
2016-02-25 23:05:14 -0800

    Distinguish b/w prod and dev when running listener.py, and prepend lib/ path so its libs take priority. Test plan: none

--- bot_globals.py
+++ bot_globals.py
@@ -1,6 +1,14 @@
 """Global vars set at startup."""
 import os
+import sys
 
-# STOPSHIP: figure out how to toggle this when starting listener
-is_dev_server = (os.environ.get("SERVER_SOFTWARE", "Development")
-                 .startswith('Development'))
+if len(sys.argv) > 0 and sys.argv[0] == "listener.py":
+    # If we're running the listener, check if we're running in dev mode
+    # TODO(kamens): this is crude argument parsing logic, if this gets extended
+    # we should use argparse
+    is_dev_server = len(sys.argv) < 2 or sys.argv[1] != "--prod"
+else:
+    # If we're running app engine, check if we're running on dev server
+    is_dev_server = (
+            os.environ.get("SERVER_SOFTWARE", "Development").startswith(
+                "Development"))
